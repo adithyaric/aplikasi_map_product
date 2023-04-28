cafeda96be64dc5800d7f5c50429cfc20925b33c
adithyaric <naughtyadit@gmail.com>
2023-04-28 23:47:19 +0700

    add adminLte2

---
+++ app/Http/Controllers/FacultyController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Faculty;
+use Illuminate\Http\Request;
+
+class FacultyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $faculties = Faculty::get();
+
+        return view('faculty.index', compact('faculties'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Faculty $faculty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Faculty $faculty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Faculty $faculty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Faculty $faculty)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,33 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset('img/logo.png') }}" class="img-circle" alt="User Image"><br>
+            </div>
+            <div class="pull-left info">
+                <p>{{ Auth::user()->name }}</p>
+                <p>Puskesmas Gondosari</p>
+                <!-- Status -->
+                {{-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a> --}}
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            <!-- Optionally, you can add icons to the links -->
+            <li class="active"><a href="/dashboard"><i class="fa fa-tachometer"></i> <span>Dashboard</span></a></li>
+            <li class="treeview">
+                <a href="#"><i class="fa fa-server"></i> <span>Master Data</span> <i
+                        class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    {{-- <li><a href="{{ route('users.index') }}"><i class="fa fa-circle-o"></i> User</a></li> --}}
+                </ul>
+            </li>
+        </ul><!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
